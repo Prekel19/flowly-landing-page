f5eb3944b2829ee285c396be7f7dd62e88952b81
Prekel19 <wiktornicolasbalamut@gmail.com>
2024-10-14 16:52:20 +0200

    Little changes in 404 page.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,26 +3,26 @@ import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "404",
-  description: "Page not found",
+  description: "Nie znaleziono strony",
 };
 
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center font-noto text-center w-full h-screen">
-      <h3 className="text-base font-bold uppercase">Oops! Page not found</h3>
+      <h3 className="text-base font-bold uppercase">Oops! Nie znaleziono strony</h3>
       <h1 style={{ lineHeight: "85%" }} className="text-10xl font-black">
         404
       </h1>
       <p className="text-lg font-light uppercase">
-        We are sorry, but the page
+        Przepraszamy, ale strona której
         <br />
-        you requested was not found
+        szukasz nie istnieje
       </p>
       <Link
         href="/"
         className="flex py-2 px-4 border border-black rounded-xl mt-6 hover:bg-black hover:text-white transition"
       >
-        Back to home
+        Wróć na stronę główną
       </Link>
     </div>
   );
